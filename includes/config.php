11e49c1221ce23d6d493db640038334b02f4d168
Sylvain Costard <sylvain.costard@uhb.fr>
2015-01-12 13:41:34 +0100

    Config par defaut

--- includes/config.php
+++ includes/config.php
@@ -2,29 +2,21 @@
 
 // DB
 
-/*
-$config["sql_db"]     = "test";
 $config["sql_server"]   = "localhost";
+$config["sql_db"]       = "db";
 $config["sql_port"]     = "3306";
-$config["sql_login"]    = "root";
-$config["sql_password"] = "@BB98-";
-*/
-
-$config["sql_server"]   =  "sql.olympe.in";
-$config["sql_db"]       = "K1Cq5R2m";
-$config["sql_port"]     = "3306";
-$config["sql_login"]    = "K1Cq5R2m";
-$config["sql_password"] = "club35";
+$config["sql_login"]    = "user";
+$config["sql_password"] = "pass";
 
 
 // Mail
-$config["mail_from"] = "clubrennesaquario@free.fr";
-$config["mail_name"] = "Club Rennais Aquariophile";
+$config["mail_from"] = "aaa@free.fr";
+$config["mail_name"] = "Club Aquariophile";
 
 
 // Site
 $config["Evenement"] = "Bourse Aquariophile 2012";
-$config["Url"] = "http://clubrennaisaquariophile.org/bourse/";
+$config["Url"] = "https://xxxxx/bourse/";
 
 ?>
 
